9268be39bd38d3db1c1eca5f9ba8f3fd8659df43
Hwuiwon Kim <hwuiwon.kim@gmail.com>
2023-08-30 16:48:27 -0400

    generate main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,14 @@
 
 import React from 'react';
 
+import Header from '@/components/Common/Header';
+import Hero from '@/components/Main/Hero';
+
 export default function MainPage() {
-  return <div className="min-h-screen"></div>;
+  return (
+    <div className="min-h-screen">
+      <Header />
+      <Hero />
+    </div>
+  );
 }
