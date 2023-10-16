2ae69e482c3e7022333993cdb53d19a980080d76
nikoskiouris <nikoskiouris@gmail.com>
2023-10-16 18:47:06 -0400

    Added confirm password to registration

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -16,7 +16,7 @@ export default function Login() {
               alt="Logo"
             />
           </Link>
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-grey-900">
             Sign in to your account
           </h2>
         </div>
