043fd6e3fd282ff6be5c42b5a0cd039830761441
nikoskiouris <nikoskiouris@gmail.com>
2023-10-16 16:47:09 -0400

    Added status to appointment page

--- src/app/appointment/page.tsx
+++ src/app/appointment/page.tsx
@@ -13,18 +13,37 @@ const appointmentDummy = [
     date_time: '11/11/2024 08:00AM',
     patient_name: 'Leslie Alexander',
     patient_id: '1984729182',
+    status: 'confirmed', // Add the "status" property
   },
-  // More people...
+  // Add more appointment objects with the "status" property...
 ];
+// ... (Previous code remains unchanged)
+
+// Add status icons and sorting feature
 
 export default function Appointment() {
-
-  {
   const [openAddApptModal, setOpenAddApptModal] = useState<boolean>(false);
   const [patient, setPatient] = useState<string>('');
   const [date, setDate] = useState<string>('');
   const [time, setTime] = useState<string>('');
 
+  // Additional features
+  const [sortOrder, setSortOrder] = useState<'asc' | 'desc'>('asc');
+  const [filteredAppointments, setFilteredAppointments] =
+    useState(appointmentDummy);
+  const [statusFilter, setStatusFilter] = useState<string | null>(null);
+
+  const toggleSortOrder = () => {
+    setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
+  };
+
+  const sortAppointments = (a, b) => {
+    if (sortOrder === 'asc') {
+      return a.date_time.localeCompare(b.date_time);
+    } else {
+      return b.date_time.localeCompare(a.date_time);
+    }
+  };
 
   useEffect(() => {
     // fetch information using patient id
@@ -51,7 +70,10 @@ export default function Appointment() {
   return (
     <React.Fragment>
       <Sidebar selectedPage={Page.APPOINTMENT} />
-      <AddAppointmentModal open={openAddApptModal} setOpen={setOpenAddApptModal} />
+      <AddAppointmentModal
+        open={openAddApptModal}
+        setOpen={setOpenAddApptModal}
+      />
       <div className="xl:pl-72 py-5">
         <div className="px-4 sm:px-6 lg:px-8">
           <div className="sm:flex sm:items-center">
@@ -60,7 +82,7 @@ export default function Appointment() {
                 Appointments
               </h1>
               <p className="mt-2 text-sm text-gray-700">
-                A list of all of your upcoming appointments.
+                A list of all your upcoming appointments.
               </p>
             </div>
             <div className="mt-4 sm:ml-16 sm:mt-0 flex flex-row">
@@ -85,6 +107,25 @@ export default function Appointment() {
               >
                 Add Appointment
               </button>
+              <div className="ml-4">
+                <button
+                  onClick={toggleSortOrder}
+                  className="block rounded-md px-3 py-2 text-center text-sm font-semibold text-gray-900 shadow-sm hover:text-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+                >
+                  {`Sort ${sortOrder === 'asc' ? 'Descending' : 'Ascending'}`}
+                </button>
+              </div>
+              <div className="ml-4">
+                <select
+                  onChange={e => setStatusFilter(e.target.value)}
+                  className="block rounded-md px-3 py-2 text-center text-sm font-semibold text-gray-900 shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+                >
+                  <option value="">All Status</option>
+                  <option value="confirmed">Confirmed</option>
+                  <option value="pending">Pending</option>
+                  <option value="canceled">Canceled</option>
+                </select>
+              </div>
             </div>
           </div>
           <div className="mt-8 flow-root">
@@ -102,7 +143,8 @@ export default function Appointment() {
                         </th>
                         <th
                           scope="col"
-                          className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                          className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900 cursor-pointer"
+                          onClick={toggleSortOrder}
                         >
                           Date & Time
                         </th>
@@ -127,31 +169,55 @@ export default function Appointment() {
                       </tr>
                     </thead>
                     <tbody className="divide-y divide-gray-200 bg-white">
-                      {appointmentDummy.map(appointment => (
-                        <tr key={appointment.id}>
-                          <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
-                            {appointment.id}
-                          </td>
-                          <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-600">
-                            {appointment.date_time}
-                          </td>
-                          <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-600">
-                            {appointment.patient_id}
-                          </td>
-                          <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-600">
-                            {appointment.patient_name}
-                          </td>
-                          <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                            <Link
-                              href={`/appointment/${appointment.id}`}
-                              className="text-blue-600 hover:text-blue-900"
-                            >
-                              View
-                              <span className="sr-only">, {appointment.date_time}</span>
-                            </Link>
-                          </td>
-                        </tr>
-                      ))}
+                      {filteredAppointments
+                        .sort(sortAppointments)
+                        .filter(appointment =>
+                          statusFilter
+                            ? appointment.status === statusFilter
+                            : true
+                        )
+                        .map(appointment => (
+                          <tr key={appointment.id}>
+                            <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
+                              {appointment.id}
+                            </td>
+                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-600">
+                              {appointment.date_time}
+                              {appointment.status === 'confirmed' && (
+                                <span className="ml-2 text-green-500">
+                                  Confirmed
+                                </span>
+                              )}
+                              {appointment.status === 'pending' && (
+                                <span className="ml-2 text-orange-500">
+                                  Pending
+                                </span>
+                              )}
+                              {appointment.status === 'canceled' && (
+                                <span className="ml-2 text-red-500">
+                                  Canceled
+                                </span>
+                              )}
+                            </td>
+                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-600">
+                              {appointment.patient_id}
+                            </td>
+                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-600">
+                              {appointment.patient_name}
+                            </td>
+                            <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                              <Link
+                                href={`/appointment/${appointment.id}`}
+                                className="text-blue-600 hover:text-blue-900"
+                              >
+                                View
+                                <span className="sr-only">
+                                  , {appointment.date_time}
+                                </span>
+                              </Link>
+                            </td>
+                          </tr>
+                        ))}
                     </tbody>
                   </table>
                 </div>
@@ -162,5 +228,4 @@ export default function Appointment() {
       </div>
     </React.Fragment>
   );
-}
 }
