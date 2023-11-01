82541bcf9d09fbda3af50073f4261e83c2e083e2
Varun Hegde <varunhegde14@gmail.com>
2023-11-01 19:17:16 -0400

    Created new Upload endpoint in FastAPI

--- backend/app/services/__init__.py
+++ backend/app/services/__init__.py
@@ -1,4 +1,5 @@
 from .CognitoService import CognitoService
 from .DynamoDBService import DynamoDBService
+from .S3Service import S3Service
 
-__all__ = ["DynamoDBService", "CognitoService"]
+__all__ = ["DynamoDBService", "CognitoService", "S3Service"]
